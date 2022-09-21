5bb81775ea37d635c15e1c36e13d59f673ca641b
Prathamesh Karambelkar <83802999+prathameshkarambelkar@users.noreply.github.com>
2022-09-21 22:51:18 +0530

    initialized firebase, done with basic homesceen UI

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,31 +1,58 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Movie from "./Movie";
-import {MdChevronLeft, MdChevronRight} from 'react-icons/md'
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import { async } from "@firebase/util";
 
-const Row = ({ title, fetchURL }) => {
+const Row = ({ title, fetchURL, rowID }) => {
   const [movies, setMovies] = useState([]);
 
+  
+  const fetchMovie = async () => {
+    let response = await axios.get(fetchURL);
+    console.log(response)
+  }
+  
 
   useEffect(() => {
     axios.get(fetchURL).then((response) => {
       setMovies(response.data.results);
     });
+    fetchMovie();
   }, [fetchURL]);
 
-  console.log(movies);
+  const slideLeft = () => {
+    var slider = document.getElementById("slider" + rowID);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+
+  const slideRight = () => {
+    var slider = document.getElementById("slider" + rowID);
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
 
   return (
     <>
       <h1 className="text-white font-bold md:text-xl p-4">{title}</h1>
       <div className="relative flex items-center group">
-      <MdChevronLeft  size={40} className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden  group-hover:block'/>
-        <div id={"slider"} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative  ' > 
+        <MdChevronLeft
+          onClick={slideLeft}
+          size={40}
+          className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden  group-hover:block"
+        />
+        <div
+          id={"slider" + rowID}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative  "
+        >
           {movies.map((item, id) => (
             <Movie key={id} item={item} />
           ))}
         </div>
-        <MdChevronRight size={40} className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block '/>
+        <MdChevronRight
+          onClick={slideRight}
+          size={40}
+          className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block "
+        />
       </div>
     </>
   );
