aa3c624ef3f1b2cfa59c50d63f4647abfa361038
Prathamesh Karambelkar <83802999+prathameshkarambelkar@users.noreply.github.com>
2022-09-21 21:56:02 +0530

    Completed with scrollbar-hide and row

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,10 +1,11 @@
 import axios from "axios";
-import { FaHeart, FaRegHeart } from "react-icons/fa";
 import React, { useEffect, useState } from "react";
+import Movie from "./Movie";
+import {MdChevronLeft, MdChevronRight} from 'react-icons/md'
 
 const Row = ({ title, fetchURL }) => {
   const [movies, setMovies] = useState([]);
-  const [like, setLike] = useState(false);
+
 
   useEffect(() => {
     axios.get(fetchURL).then((response) => {
@@ -17,30 +18,14 @@ const Row = ({ title, fetchURL }) => {
   return (
     <>
       <h1 className="text-white font-bold md:text-xl p-4">{title}</h1>
-      <div className="relative flex items-center">
-        <div id={"slider"}>
+      <div className="relative flex items-center group">
+      <MdChevronLeft  size={40} className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden  group-hover:block'/>
+        <div id={"slider"} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative  ' > 
           {movies.map((item, id) => (
-            <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[200px] inline-block cursor-pointer relative p-2 ">
-              <img
-                className="w-full h-auto block"
-                src={`https://image.tmdb.org/t/p/w500/${item.backdrop_path}`}
-                alt={item?.title}
-              />
-              <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white ">
-                <p className="white-space-normal text-xs md:text-small flex justify-center h-full text-center items-center font-bold">
-                  {item?.title}
-                </p>
-                <p className="">
-                  {like ? (
-                    <FaHeart className="absolute top-4 left-4 text-gray-300 " />
-                  ) : (
-                    <FaRegHeart className="absolute top-4 left-4 text-gray-300 "  />
-                  )}{" "}
-                </p>
-              </div>
-            </div>
+            <Movie key={id} item={item} />
           ))}
         </div>
+        <MdChevronRight size={40} className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block '/>
       </div>
     </>
   );
